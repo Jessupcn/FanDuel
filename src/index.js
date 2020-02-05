8c1c120f554643659ea8f6b1944a461f00067a5d
Connor Jessup <cjessup@umich.edu>
2020-02-04 22:11:27 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App component
+import App from './App';
+
+// render App to DOM
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
